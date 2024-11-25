e12c9489fc83e3dc99d93d988747cdb17ed37c9c
kumar4532 <krantikumar2001@gmail.com>
2024-11-25 19:52:41 +0530

    edit config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,13 @@ import type { Config } from "tailwindcss";
 export default {
     darkMode: ["class"],
     content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+	 
+		// Or if using `src` directory:
+		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+	  ],
   theme: {
   	extend: {
   		colors: {
